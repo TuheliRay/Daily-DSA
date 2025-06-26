0b6412f3c2aa5514e5dbf53d835d57621ea67f6a
Tuheli Ray <tuheliray@gmail.com>
2025-06-26 15:26:32 +0530

    Two Sum Solution

---
+++ TwoSum.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer , Integer> hm= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int c=target-nums[i];
+            if(hm.containsKey(c)){
+                return new int[] {i , hm.get(c)};
+            }
+            hm.put(nums[i] , i);
+        }
+        return null;   
+    }
+}
