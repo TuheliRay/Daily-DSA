cc9dacab1314293a5892a2dc9bba3482c6abca7a
Tuheli Ray <tuheliray@gmail.com>
2025-07-31 13:27:14 +0530

    Valid Parenthesis String

---
+++ ValidParenthesis.java
@@ -0,0 +1,26 @@
+class ValidParenthesis {
+    public boolean checkValidString(String s) {
+        int max=0;
+        int min=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                min++;
+                max++;
+            }
+            else if(ch==')'){
+                min--;
+                max--;
+            }
+            else{
+                min--;
+                max++;
+            }
+            if(min<0)
+            min=0;
+            if(max<0)
+            return false;
+        }
+        return (min==0); 
+    }
+}
