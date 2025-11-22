5c1a440290e3fb756891b168d50d495a1ef752b2
Tuheli Ray <tuheliray@gmail.com>
2025-11-22 12:37:14 +0530

    Grid Unique Paths

---
+++ GridUniquePaths.java
@@ -0,0 +1,23 @@
+class GridUniquePaths {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for(int i=0 ; i<m ; i++)
+        Arrays.fill(dp[i] , -1);
+        return countPaths(m , n , dp);
+    }
+    public int countPaths(int m , int n , int dp[][]){
+        for(int i = 0 ; i<m ; i++){
+            for(int j=0 ; j<n ; j++){
+                int left=0 , up = 0;
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                if(j>0) left = dp[i][j-1];
+                if(i>0) up = dp[i-1][j];
+                dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
